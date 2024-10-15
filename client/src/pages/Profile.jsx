951751ef17c584eb23ce5787174a539b03f6f125
Charbonneau John <john.charbonneau93@gmail.com>
2024-10-14 22:14:09 -0600

    fixed signup route.

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,20 +1,61 @@
 import React from 'react';
+import { Navigate, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
-import { GET_ME } from '../utils/queries';
-import { useMutation } from '@apollo/client';
-import { SAVE_BOOK } from '../utils/mutations';
-import SavedBooks from '../components/SavedBooks'
-import SearchBooks from '../components/SearchBooks'
+import { QUERY_ME } from '../utils/queries';
+import SavedBooks from '../components/SavedBooks';
+import SearchBooks from '../components/SearchBooks';
+import Auth from '../utils/auth';
 
 const Profile = () => {
-return (
-    <section>
-        < SavedBooks />
-        < SearchBooks />
-    </section>
+  const { username: userParam } = useParams();
 
-)
-}
+  const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
+    variables: { username: userParam },
+  });
 
+  const user = data?.me || data?.user || {};
+  if (
+    Auth.loggedIn() && 
+    /* Run the getProfile() method to get access to the unencrypted token value in order to retrieve the user's username, and compare it to the userParam variable */
+    Auth.getProfile().authenticatedPerson.username === userParam
+  ) {
+    return <Navigate to="/me" />;
+  }
 
-export default Profile
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!user?.username) {
+    return (
+      <h4>
+        You need to be logged in to see this. Use the navigation links above to
+        sign up or log in!
+      </h4>
+    );
+  }
+
+  return (
+    <div>
+      <div className="flex-row justify-center mb-3">
+        <h2 className="col-12 col-md-10 bg-dark text-light p-3 mb-5">
+          Viewing {userParam ? `${user.username}'s` : 'your'} profile.
+        </h2>
+
+        <div className="col-12 col-md-10 mb-5">
+            < SearchBooks />
+        </div>
+        {!userParam && (
+          <div
+            className="col-12 col-md-10 mb-3 p-3"
+            style={{ border: '1px dotted #1a1a1a' }}
+          >
+            < SavedBooks />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
--- client/src/utils/authService.js
+++ client/src/utils/authService.js
@@ -1,37 +1,37 @@
-import decode from 'jwt-decode';
+// import decode from 'jwt-decode';
 
-class AuthService {
-  getProfile() {
-    return decode(this.getToken());
-  }
+// class AuthService {
+//   getProfile() {
+//     return decode(this.getToken());
+//   }
 
-  loggedIn() {
-    const token = this.getToken();
-    return !!token && !this.isTokenExpired(token);
-  }
+//   loggedIn() {
+//     const token = this.getToken();
+//     return !!token && !this.isTokenExpired(token);
+//   }
 
-  isTokenExpired(token) {
-    try {
-      const decoded = decode(token);
-      return decoded.exp < Date.now() / 1000;
-    } catch (err) {
-      return false;
-    }
-  }
+//   isTokenExpired(token) {
+//     try {
+//       const decoded = decode(token);
+//       return decoded.exp < Date.now() / 1000;
+//     } catch (err) {
+//       return false;
+//     }
+//   }
 
-  getToken() {
-    return localStorage.getItem('id_token');
-  }
+//   getToken() {
+//     return localStorage.getItem('id_token');
+//   }
 
-  login(idToken) {
-    localStorage.setItem('id_token', idToken);
-    window.location.assign('/');
-  }
+//   login(idToken) {
+//     localStorage.setItem('id_token', idToken);
+//     window.location.assign('/');
+//   }
 
-  logout() {
-    localStorage.removeItem('id_token');
-    window.location.assign('/');
-  }
-}
+//   logout() {
+//     localStorage.removeItem('id_token');
+//     window.location.assign('/');
+//   }
+// }
 
-export default new AuthService();
+// export default new AuthService();
