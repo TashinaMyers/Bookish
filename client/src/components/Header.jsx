b3b19ae05853b59496eb60970c6dee3897b3a5d3
Charbonneau John <john.charbonneau93@gmail.com>
2024-10-16 21:05:34 -0600

    adding in basic styling.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -12,9 +12,9 @@ const Header = () => {
       <div className="container flex-row justify-space-between-lg justify-center align-center">
         <div>
           <Link className="text-light" to="/">
-            <h1 className="m-0">Tech Thoughts</h1>
+            <h1 className="m-0">Bookish</h1>
           </Link>
-          <p className="m-0">Get into the mind of a programmer.</p>
+          <p className="m-0">Search books to your hearts desire!</p>
         </div>
         <div>
           {Auth.loggedIn() ? (
